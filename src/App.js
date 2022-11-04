a041c9e95406dfd8a7be2fbd6046a453122dd5b4
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-11-04 12:56:10 +0700

    learn using Navlink and Navigate in react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,23 +6,28 @@ import HomeTemplate from "./Templates/HomeTemplate";
 import HomePage from "./Pages/Home/HomePage";
 import ReactForm from "./Pages/ReactForm/ReactForm";
 import Shop from "./Pages/Shop/Shop";
+import AdminTemplate from "./Templates/AdminTemplate";
+import Users from "./Pages/Admin/Users/Users";
+import Products from "./Pages/Admin/Products/Products";
 function App() {
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
         <Routes>
-          <Route path="/shoeshop" element={<Home />} />
-          
           {/* Learn React-Router-Dom New Version */}
           <Route path="" element={<HomeTemplate />}>
-             {/* outlet => gõ đúng path name thì nó sẽ render vào đúng trong component HomeTemplate, không cần phải định nghĩa HOC nữa*/}
+            {/* outlet => gõ đúng path name thì nó sẽ render vào đúng trong component HomeTemplate, không cần phải định nghĩa HOC nữa*/}
+            <Route path="/shoeshop" element={<Home />} />
             <Route path="home" element={<HomePage />} />
             <Route path="/reactform" element={<ReactForm />} />
             <Route path="shop" element={<Shop />} />
             <Route path="*" element={<Navigate to={"home"} />} />
-
           </Route>
 
+          <Route path="admin" element={<AdminTemplate/>}>
+          <Route path="user" element={<Users/>}/>
+          <Route path="product" element={<Products/>}/>
+          </Route>
         </Routes>
       </Container>
     </BrowserRouter>
