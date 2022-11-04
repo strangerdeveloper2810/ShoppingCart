8e64aca70153d4e421f05d2cf95fec3efb2230c1
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-11-04 16:06:12 +0700

    learn using hook useSearchParam in react-router-dom

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import AdminTemplate from "./Templates/AdminTemplate";
 import Users from "./Pages/Admin/Users/Users";
 import Products from "./Pages/Admin/Products/Products";
 import Detail from "./Pages/Detail/Detail";
+import Search from "./Pages/Search/Search";
 
 export const history = createBrowserHistory({ window });
 function App() {
@@ -27,13 +28,15 @@ function App() {
           {/* Learn React-Router-Dom New Version */}
           <Route path="" element={<HomeTemplate />}>
             {/* outlet => gõ đúng path name thì nó sẽ render vào đúng trong component HomeTemplate, không cần phải định nghĩa HOC nữa*/}
-            <Route path="/shoeshop" element={<Home />} />
+            <Route path="shoeshop" element={<Home />} />
             <Route path="home" element={<HomePageWithNavigate />} />
-            <Route path="/reactform" element={<ReactForm />} />
+            <Route path="reactform" element={<ReactForm />} />
             <Route path="shop" element={<Shop />} />
             <Route path="detail">
               <Route path=":id" element={<Detail/>}/>
             </Route>
+
+            <Route path="search" element={<Search/>}/>
             <Route path="*" element={<Navigate to={"home"} />} />
           </Route>
 
